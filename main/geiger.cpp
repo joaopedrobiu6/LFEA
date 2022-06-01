add73b9ae6af9c909e1d85d0adc6f85d04826d97
joaopedrobiu6 <joaopedrofbiu@tecnico.ulisboa.pt>
2022-06-01 19:24:40 +0100

    geiger3

--- main/geiger.cpp
+++ main/geiger.cpp
@@ -22,20 +22,20 @@
 #include <fstream> // std::fstream
 #include <iomanip>
 
-void HistMaker(std::vector<std::vector<float>> data, bool wapp);
+void GraphMaker(std::vector<std::vector<float>> data, bool wapp);
 std::vector<std::vector<float>> ReadData(std::string filename);
 
 int main()
 {
     std::vector<std::vector<float>> data = ReadData("gmdist.txt");
-    HistMaker(data, true);
+    GraphMaker(data, true);
     /* hist2 lab1("countsgeiger.txt");
     lab1.MakeHist("Estatística de contagens\nDetetor Geiger Muller", "geigercounts.png", "Contagens", "Frequencia", 20, 1780, 1950, false, "gaus");
      */
     return 0;
 }
 
-void HistMaker(std::vector<std::vector<float>> data, bool wapp)
+void GraphMaker(std::vector<std::vector<float>> data, bool wapp)
 {
     TApplication app("app", NULL, NULL);
     TCanvas c("canvas", "histogram", 0, 0, 1280, 720);
@@ -51,53 +51,21 @@ void HistMaker(std::vector<std::vector<float>> data, bool wapp)
     {
         x[i] = data[i][0];
         y[i] = data[i][1];
-        ex[i] = data[i][2];
+        ex[i] = sqrt(0.015875 * 0.015875 + data[i][0] * data[i][0]) - data[i][0];
         ey[i] = data[i][3];
     }
 
     TGraphErrors gr(n, x, y, ex, ey);
-    gr.SetTitle("Cs-137 - Variacao das\ncontagens com a distancia;Dist. [m];Contagens");
+    gr.SetTitle("Cs-137 - Variac#tilde{a}o do n#acute{u}mero contagens com a dist#hat{a}ncia;Dist. [m];Contagens");
     gr.SetMarkerColor(4);
     gr.SetMarkerStyle(21);
 
-    TF1 *f = new TF1("func", "[0]/(x*x) + [1]", 0, 6);
+    TF1 *f = new TF1("func", "[0]*(1/(x*x)) + [1]", 0, 6);
     gr.Fit(f);
-    
+
     c.Update();
     gr.Draw("AP");
     app.Run("true");
-
-    /*         h.SetFillColor(kBlue);
-        h.SetMarkerStyle(kFullDiamond);
-        h.SetMarkerColor(kBlue);
-        h.SetLineColor(1);
-
-        gStyle->SetStatY(0.85);
-        gStyle->SetStatX(0.35);
-
-        h.SetTitle("Variacao com a Distancia");
-        h.GetXaxis()->SetTitle("dist [m]");
-        h.GetYaxis()->SetTitle("Contagens");
-        h.Smooth();
-
-        for (int i = 0; i < data.size(); i++)
-        {
-            j
-            h.SetBinContent(data[i][0], data[i][1]);
-        }
-
-        h.Draw("E1");
-
-        if (wapp == true)
-        {
-
-            app.Run("true");
-        }
-        if (wapp == false)
-        {
-            c.Update();
-            c.SaveAs("gmhist.pdf");
-        } */
 };
 
 std::vector<std::vector<float>> ReadData(std::string filename)
