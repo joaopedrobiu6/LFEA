b892725199e90046875224085fd4d90c99a5bada
joaopedrobiu6 <joaopedrofbiu@tecnico.ulisboa.pt>
2022-09-26 11:59:09 +0100

    okay i pull up

--- main/pet.cpp
+++ main/pet.cpp
@@ -3,6 +3,8 @@
 int main()
 {
     auto data = ReadData("PET1.txt");
+    for (int i = 0; i < data.size(); i++)
+        data[i][2] = data[i][2] * 5;
     GraphMaker(data, "Correlac#tilde{o}es Angulares #gamma-#gamma;#theta [^{o}];Rates [cts/s]", "", "AP", 60);
 
     return 0;
