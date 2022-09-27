cceacccd1e9ebb8bcc71d24e5bf26e67619545a5
joaopedrobiu6 <joaopedrofbiu@tecnico.ulisboa.pt>
2022-09-27 10:56:30 +0100

    gt3 gt3 quero o brancooo

--- main/pet.cpp
+++ main/pet.cpp
@@ -2,10 +2,14 @@
 
 int main()
 {
-    auto data = ReadData("PET1.txt");
+    /* auto data = ReadData("PET1.txt");
     for (int i = 0; i < data.size(); i++)
         data[i][2] = data[i][2] * 5;
     GraphMaker(data, "Correlac#tilde{o}es Angulares #gamma-#gamma;#theta [^{o}];Rates [cts/s]", "", "AP", 60);
-
+ */
+    auto data = ReadData("NA22.txt");
+    for (int i = 0; i < data.size(); i++)
+        data[i][3] = 0;
+    HistMaker(data, true, 1024, 0, 1023, "Espetro Na-22;CHN;Counts", "na22.png", "P");
     return 0;
 }
